31b81061930ff96e12ea0818729fb2195c7e2188
monocosmico <ancientc0der@outlook.com>
2024-11-01 14:06:19 -0600

    Updates

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useActiveAccount } from "thirdweb/react";
-import thirdwebIcon from "@public/thirdweb.svg";
+import vemeIcon from "@public/alphalogo.png";
 import { shortenAddress } from "thirdweb/utils";
 import { Button } from "@headlessui/react";
 import { client, wallet } from "./constants";
@@ -29,7 +29,7 @@ export default function Home() {
             )}      
         </div>
 
-        <Menu />
+        {/* <Menu /> */}
       </div>
     </main>
   );
@@ -39,27 +39,27 @@ function Header() {
   return (
     <header className="flex flex-col items-center mb-20 md:mb-20">
       <Image
-        src={thirdwebIcon}
+        src={vemeIcon}
         alt=""
-        className="size-[150px] md:size-[150px]"
+        // className="size-[250px] md:size-[250px]"
         style={{
           filter: "drop-shadow(0px 0px 24px #a726a9a8)",
         }}
       />
 
       <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        thirdweb SDK
+        {/* Veme Alpha Whitelist */}
         <span className="text-zinc-300 inline-block mx-1"> + </span>
         <span className="inline-block text-blue-500"> Telegram </span>
       </h1>
 
-      <p className="text-zinc-300 text-base">
+      {/* <p className="text-zinc-300 text-base">
         Read the{" "}
         <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
           README.md
         </code>{" "}
         file to get started.
-      </p>
+      </p> */}
     </header>
   );
 }
